c2127f538d0a3d99e1861f32aed2d8f0517f9fce
RSJ <rlstjohn@gmail.com>
2026-06-17 11:32:07 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+/* Currency converter service worker */
+const CACHE = "currency-v1";
+const SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./icon-maskable-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  const url = new URL(e.request.url);
+  // Let cross-origin rate APIs (er-api.com, jsdelivr) go straight to the network.
+  if (url.origin !== self.location.origin) return;
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+});
