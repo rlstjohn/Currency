eb0650d2fbd118fe58839b0e01187c31ce75d5b8
RSJ <rlstjohn@gmail.com>
2026-06-19 00:16:50 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,29 +1,8 @@
-/* Currency converter service worker */
-const CACHE = "currency-v1";
-const SHELL = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png",
-  "./icon-maskable-512.png"
-];
-
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)).then(() => self.skipWaiting()));
+var CACHE = 'currency-v1';
+var FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(FILES); }));
 });
-
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys()
-      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch", (e) => {
-  const url = new URL(e.request.url);
-  // Let cross-origin rate APIs (er-api.com, jsdelivr) go straight to the network.
-  if (url.origin !== self.location.origin) return;
-  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+self.addEventListener('fetch', function(e) {
+  e.respondWith(caches.match(e.request).then(function(r) { return r || fetch(e.request); }));
 });
